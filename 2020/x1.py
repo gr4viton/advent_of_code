90a32da62c499437344ad45e649aa5377e9a3ffe
gr4viton <lordmutty@gmail.com>
2020-12-13 00:09:36 +0100

    start - but the loader is offline? maan

--- 2020/x1.py
+++ 2020/x1.py
@@ -1,7 +1,17 @@
 from aocd import data
+from aocd import Puzzle
 
+from dataclasses import dataclass
 
-# puzzle = Puzzle(year=2017, day=20)
+@dataclass
+class PuzzleFactory:
+    year: int
+    day: int
+
+    def get_puzzle(self):
+        puzzle = Puzzle(year=self.year, day=self.day)
+        return puzzle
+
 
 # from aocd import submit
 # my_answer = 42
@@ -13,3 +23,15 @@ from aocd import data
 # puzzle
 # # <Puzzle(2017, 20) at 0x107322978 - Particle Swarm>
 # puzzle.input_data
+
+# class SolverX1:
+def main(puzzle):
+    print("start")
+    pass
+
+
+if __name__ == "__main__":
+
+    puzzle = None
+    # puzzle = PuzzleFactory(2020, 1).get_puzzle()
+    main(puzzle)
