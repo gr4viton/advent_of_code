037052a32f2a0519366e5f1fca919ab3f2694057
gr4viton <lordmutty@gmail.com>
2020-12-12 21:06:38 +0100

    before even.

---
+++ 2020/x1.py
@@ -0,0 +1,15 @@
+from aocd import data
+
+
+# puzzle = Puzzle(year=2017, day=20)
+
+# from aocd import submit
+# my_answer = 42
+# submit(my_answer, part="a", day=25, year=2017)
+
+
+# from aocd.models import Puzzle
+# puzzle = Puzzle(year=2017, day=20)
+# puzzle
+# # <Puzzle(2017, 20) at 0x107322978 - Particle Swarm>
+# puzzle.input_data
